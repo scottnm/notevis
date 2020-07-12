bfd00383c48173666781e06b3ce6a29053d4e0a0
Scott Munro <scottnmunro@gmail.com>
2020-07-11 19:53:56 -0700

    idea

--- notevis.py
+++ notevis.py
@@ -4,6 +4,7 @@ ideas:
     - allow different string counts
     - allow different fret "views"
     - pick unique colors for different notes highlighted
+    - support sharps and flats
 """
 
 def print_string(string, header = ""):
@@ -12,6 +13,24 @@ def print_string(string, header = ""):
 # print a single string (with a fret header)
 #    | 01 | 02 | 03 | 04 |
 # G. | -- | -- | -- | -- |
+
+@enum.unique
+class Note(enum.Enum):
+    A  = 1
+    AB = 2
+    B  = 3
+    C  = 4
+    CD = 5
+    D  = 6
+    DE = 7
+    E  = 8
+    F  = 9
+    FG = 10
+    G  = 11
+
+def generate_string(open_note):
+    for fret in range(1, fret_count + 1):
+    pass
 
 # print the header
 fret_count = 17
